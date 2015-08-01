479f1d035a2ea889781828f0071f4986722b62cc
James Gowans <gowans.james@gmail.com>
2015-08-01 16:40:47 +0200

    Correlation to have snap and get frequency
    The Correlation class initialises and contains a snapshot block for
    abstracing getting the signal from the FPGA. Also, method to try to
    extract the frequency bin of interest.

--- directionFinder_backend/correlation.py
+++ directionFinder_backend/correlation.py
@@ -8,17 +8,30 @@ import numpy as np
 from snapshot import Snapshot
 
 class Correlation:
-    def __init__(self, f_start, f_stop, logger=logging.getLogger(__name__)):
+    def __init__(self, fpga, comb, f_start, f_stop, logger=logging.getLogger(__name__)):
         self.logger = logger
-        self.correlations = {}
+        snap_name = "snap_{a}x{b}".format(a=comb[0], b=comb[1])
+        self.snapshot = Snapshot(fpga,
+                                 snap_name,
+                                 dtype='>i4',
+                                 cvalue=True,
+                                 logger=self.logger.getChild(snap_name))
         self.f_start = f_start
         self.f_stop = f_stop
 
-    def update(self, comb, signal):
-        self.correlations[comb] = signal
+    def arm(self):
+        self.snapshot.arm()
 
-    def phase_difference_at_freq(self, to_compare, f):
-        total = 0
-        for corr0_comb, corr0_sig in corr0.items():
+    def fetch_signal(self, force=False):
+        self.snapshot.fetch_signal(force)
+
+    def phase_at_freq(self, f):
+        """ Note: this formula may need fixing!! Check against actual data
+        """
+        fft_bin_number = int(round(
+            f-self.f_start * (self.f_stop - self.f_start) / (len(self.signal))
+        ))
+        fft_bin = self.snapshot.signal[fft_bin_number]
+        return np.phase(fft_bin)
 
 
