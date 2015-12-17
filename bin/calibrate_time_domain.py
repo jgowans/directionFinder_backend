33641c7266884908f419bb50cad522659d7edd3b
James Gowans <gowans.james@gmail.com>
2015-12-17 18:37:46 +0200

    Time calibrator and plotter complete
    Shows time different with zoomed insert (woot!) as well as writes cal
    values to json file.

--- bin/calibrate_time_domain.py
+++ bin/calibrate_time_domain.py
@@ -9,6 +9,8 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 import logging
 import time
 import timeit
+import json
+import datetime
 
 def test_cross_first_vs_resample_first():
     c.upsample_factor = 100
@@ -85,29 +87,50 @@ def do_calibration(c):
     offsets = {}
     for baseline, correlation in c.time_domain_correlations.items():
         argmax = np.argmax(correlation)
-        offset = c.time_domain_correlations_sample_times
+        offset = c.time_domain_correlations_sample_times[argmax]
+        # I'd much prefer to use a tuple as key, but JSON doens't support it.
+        baseline_str = "{a}{b}".format(a = baseline[0], b = baseline[1])
+        offsets[baseline_str] = offset
+    offsets["metadata"] = {}
+    offsets["metadata"]["created"] = datetime.datetime.utcnow().isoformat("T")
+    offsets_json = json.dumps(offsets, indent=2)
+    with open('time_domain_calibration.json', 'w') as f:
+        f.write(offsets_json)
+
 
 def plot_calibration(c):
     fig = plt.figure()
     ax = plt.gca()
+    normaliser = max(c.time_domain_correlations[(0,1)])
     for baseline, correlation in c.time_domain_correlations.items():
         correlation_max_val = correlation[np.argmax(correlation)]
         correlation_max_time = c.time_domain_correlations_sample_times[np.argmax(correlation)]
         lines = ax.plot(
-            c.time_domain_correlations_sample_times,
-            correlation,
+            c.time_domain_correlations_sample_times * 1e6,
+            correlation / normaliser,
             label = baseline)
-        ax.plot([correlation_max_time, correlation_max_time], [0, correlation_max_val], color = lines[0].get_color())
-    axins = zoomed_inset_axes(ax, 6, loc=1)
+        #ax.plot([correlation_max_time, correlation_max_time], [0, correlation_max_val], color = lines[0].get_color())
+    axins = zoomed_inset_axes(ax, 7, loc=2)
     for baseline, correlation in c.time_domain_correlations.items():
         correlation_max_val = correlation[np.argmax(correlation)]
         correlation_max_time = c.time_domain_correlations_sample_times[np.argmax(correlation)]
-        lines = ax.plot(
-            c.time_domain_correlations_sample_times,
-            correlation,
-            label = baseline)
-        axins.plot([correlation_max_time, correlation_max_time], [0, correlation_max_val], color = lines[0].get_color())
+        lines = axins.plot(
+            c.time_domain_correlations_sample_times * 1e6,
+            correlation / normaliser,
+            label = baseline,
+            linewidth=2)
+        #axins.plot([correlation_max_time, correlation_max_time], [0, correlation_max_val], color = lines[0].get_color())
+    axins.set_xlim(-0.7e-3, 0.7e-3)
+    axins.set_ylim(0.95, 1.08)
+
+    plt.xticks(visible=False)
+    plt.yticks(visible=False)
+    mark_inset(ax, axins, loc1=1, loc2=4, fc='none', ec='0.5')
+    ax.set_title("Time domain cross correlation for all four\n baselines with broad band noise arriving through full RF chain")
+    ax.set_xlabel("Time delay (us)")
+    ax.set_ylabel("Cross correlation value (normalised)")
     ax.legend()
+    #ax.legend()
     plt.show()
 
 if __name__ == '__main__':
