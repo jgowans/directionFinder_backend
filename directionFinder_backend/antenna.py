Fixed antenna rotation direction

directionFinder_backend/antenna.py
@@ -22,7 +22,9 @@ class Antenna:
         return self.rotated(phi).x
 
     def phase_at_angle(self, phi, f, c=scipy.constants.c):
-        new_distance = self.rotated_distance(phi)
+        # rotate negative because the signal source going one way around is the same
+        # as the antenna array going the other way.
+        new_distance = self.rotated_distance(-phi)
         new_phase = new_distance * (f/c) * 2*np.pi
         # force phase to range from -pi to pi. I sort of know how this works...
         normalised_phase = np.arctan2(np.sin(new_phase), np.cos(new_phase))