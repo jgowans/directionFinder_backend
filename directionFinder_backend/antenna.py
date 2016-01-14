389bbb70d6ea48760f63cb1faafcaf86e9a185a3
James Gowans <gowans.james@gmail.com>
2016-01-14 23:07:13 +0200

    Rather do angle negating in shared call

--- directionFinder_backend/antenna.py
+++ directionFinder_backend/antenna.py
@@ -19,12 +19,12 @@ class Antenna:
         return Antenna(new_location[0,0], new_location[1,0])
 
     def rotated_distance(self, phi):
-        return self.rotated(phi).x
-
-    def phase_at_angle(self, phi, f, c=scipy.constants.c):
         # rotate negative because the signal source going one way around is the same
         # as the antenna array going the other way.
-        new_distance = self.rotated_distance(-phi)
+        return self.rotated(-phi).x
+
+    def phase_at_angle(self, phi, f, c=scipy.constants.c):
+        new_distance = self.rotated_distance(phi)
         new_phase = new_distance * (f/c) * 2*np.pi
         # force phase to range from -pi to pi. I sort of know how this works...
         normalised_phase = np.arctan2(np.sin(new_phase), np.cos(new_phase))
